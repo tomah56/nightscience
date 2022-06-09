8d059da5f3b10221911c4fae2a101c8efe2d90de
Tobias Grossmann <tgrossma@3-D-2.42heilbronn.de>
2022-06-09 12:57:50 +0200

    fixed ?

--- tob/canvas1.js
+++ tob/canvas1.js
@@ -30,6 +30,8 @@ function randomInt(min, max){
 	return Math.floor(Math.random()*(max-min+1)+min)
 }
 
+
+
 function drawSpiral(myPath, position, offset, width, height) {
 
 	myPath.strokeColor = 'white';
